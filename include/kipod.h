11fceb9dcdba40b431619518799000e8dce7f890
René Rühr <c@rene.ruhr>
2021-01-30 00:24:32 +0200

    Added Unified header kipod.h for project.

---
+++ include/kipod.h
@@ -0,0 +1,16 @@
+#ifndef KIPOD_H
+#define KIPOD_H
+
+#include "../vendor/entt/src/entt.hpp"
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include "utils/vec.h"
+#include "utils/mat.h"
+#include "utils/log.h"
+
+#include <core.h>
+
+
+#endif // KIPOD_H
