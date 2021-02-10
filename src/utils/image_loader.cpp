b9fae5d0375ef90b4be5184dd3cca85a2e7b1704
René Rühr <c@rene.ruhr>
2021-02-10 23:44:25 +0200

    Moving stb from main.cpp

---
+++ src/utils/image_loader.cpp
@@ -0,0 +1,18 @@
+#include "image_loader.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "../../vendor/stb/stb_image.h"
+
+Image *ImageLoader::LoadImage(const char path[])
+{
+        Image* new_image = new Image();
+        stbi_set_flip_vertically_on_load(1);
+        new_image->data_ = stbi_load(path, &new_image->width_,
+                              &new_image->height_, &new_image->n_channels_, 3);
+        return new_image;
+
+}
+
+void ImageLoader::FreeImage(Image *old_image)
+{
+        stbi_image_free(old_image->data_);
+}
--- src/utils/image_loader.h
+++ src/utils/image_loader.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "../../vendor/stb/stb_image.h"
-
 struct Image{
     int width_;
     int height_;
@@ -16,18 +14,7 @@ struct Image{
 class ImageLoader{
 public:
 
-static Image* LoadImage(const char path[])
-{
-    Image* new_image = new Image();
-    stbi_set_flip_vertically_on_load(1);
-    new_image->data_ = stbi_load(path, &new_image->width_,
-                          &new_image->height_, &new_image->n_channels_, 3);
-    return new_image;
-}
+static Image* LoadImage(const char path[]);
 
-static void FreeImage(Image* old_image)
-{
-    stbi_image_free(old_image->data_);
-}
-
+static void FreeImage(Image* old_image);
 };
