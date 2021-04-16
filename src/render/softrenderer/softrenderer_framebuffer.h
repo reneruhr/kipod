c6c77c36a04e88b07beb79638f92caeff933355d
reneruhr <c@rene.ruhr>
2021-04-16 16:33:55 +0300

    Moved definition to cpp

--- src/render/softrenderer/softrenderer_framebuffer.h
+++ src/render/softrenderer/softrenderer_framebuffer.h
@@ -17,54 +17,11 @@ public:
         buffer_(std::make_unique<float[]>(width*height*3) ),
         width_(width), height_(height){}
 
-    void Clear(){
-        for(int i = 0; i< width_*height_*3; i++) buffer_[i]=0;
-    }
-
-    float* Data(){
-        return buffer_.get();
-    }
-
-    void DrawPoint(int x, int y)
-    {
-        if( x < width_ && y < height_ && x>=0 && y>=0){
-            buffer_[INDEX(width_,x,y,0)]=1;
-            buffer_[INDEX(width_,x,y,1)]=1;
-            buffer_[INDEX(width_,x,y,2)]=1;
-        }
-    }
-
-    void DrawStraightLine(int *p, int *q)
-    {
-
-
-        if(p[0] == q[0] && p[0]<width_ && p[0]>=0){
-            if(p[1] > q[1]) std::swap(p,q);
-
-            for( int i = std::max(p[1],0); i<= std::min(q[1],height_-1) ; i++)
-            {
-                buffer_[INDEX(width_,p[0],i,0)]=1;
-                buffer_[INDEX(width_,p[0],i,1)]=1;
-                buffer_[INDEX(width_,p[0],i,2)]=1;
-            }
-
-        } else if(p[1] == q[1] && p[1]<height_ && p[1]>=0){
-            if(p[0] > q[0]) std::swap(p,q);
-            for( int i = std::max(p[0],0); i<= std::min(q[0],width_-1); i++)
-            {
-                buffer_[INDEX(width_,i,p[1],0)]=1;
-                buffer_[INDEX(width_,i,p[1],1)]=1;
-                buffer_[INDEX(width_,i,p[1],2)]=1;
-            }
-        }
-    }
-
-    void DrawPointWithColor(int x, int y, float* c)
-    {
-        buffer_[INDEX(width_,x,y,0)]=*c;
-        buffer_[INDEX(width_,x,y,1)]=*(c+1);
-        buffer_[INDEX(width_,x,y,2)]=*(c+2);
-    }
+    void Clear();
+    float* Data();
+    void DrawPoint(int x, int y);
+    void DrawStraightLine(int *p, int *q);
+    void DrawPointWithColor(int x, int y, float* c);
 };
 
 }
