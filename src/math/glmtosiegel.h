72050e62ebd847615d309eedb738b5d0a4e69bf4
reneruhr <c@rene.ruhr>
2021-04-16 16:35:01 +0300

    glm to siegel conversion

---
+++ src/math/glmtosiegel.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "../siegel2021.h"
+#include "glm/vec3.hpp"
+using Vec3f = siegel2021::Vector<float,3>;
+using Vec3d = siegel2021::Vector<double,3>;
+
+Vec3d ToSiegel(glm::vec3 vec){
+    return {vec.x,vec.y,vec.z};
+}
