210b821a90becf9ce424d4795c3cfb584461861c
reneruhr <c@rene.ruhr>
2021-09-19 17:30:44 +0300

    Fixed bug in normal calculation.

--- src/modules/meshmodels/meshmodel_parsing.cpp
+++ src/modules/meshmodels/meshmodel_parsing.cpp
@@ -4,9 +4,9 @@
 namespace kipod::MeshModels{
 
 void LoadFile(std::filesystem::path path,
-              std::shared_ptr<std::vector<vec3> > vertices_vector, std::shared_ptr<std::vector<unsigned int> > indices_vector,
-              std::shared_ptr<std::vector<vec3> > normals_vector, std::shared_ptr<std::vector<unsigned int> > nindices_vector,
-              std::shared_ptr<std::vector<vec2> > texture_vector, std::shared_ptr<std::vector<unsigned int> > tindices_vector)
+              std::shared_ptr<std::vector<Vec3> > vertices_vector, std::shared_ptr<std::vector<unsigned int> > indices_vector,
+              std::shared_ptr<std::vector<Vec3> > normals_vector, std::shared_ptr<std::vector<unsigned int> > nindices_vector,
+              std::shared_ptr<std::vector<Vec2> > texture_vector, std::shared_ptr<std::vector<unsigned int> > tindices_vector)
               {
     LOG_ENGINE("Start loading MeshModel from File.");
 
@@ -75,19 +75,45 @@ void LoadFile(std::filesystem::path path,
     }
 }
 
+bool isClose(const float& a, const float& b){
+    return std::abs(a-b) <= std::numeric_limits<float>::epsilon();
+}
+
+bool isRelClose(const float& a, const float& b){
+    return std::abs(a-b) <= std::max(std::abs(a),std::abs(b))
+                           *std::numeric_limits<float>::epsilon();
+}
+
+bool isRelLess(const float& a, const float& b){
+    return a < b + std::numeric_limits<float>::epsilon();
+}
+
+bool isClose(const Vec3& v, const Vec3& w){
+    return isClose(v.x,w.x) &&
+           isClose(v.y,w.y) &&
+           isClose(v.z,w.z);
+}
+
+bool lessVector(const Vec3& v, const Vec3& w){
+    if(isClose(v,w)) return false;
+    else return isRelLess(v.x,w.x)
+            ||  (isClose(v.x,w.x) && isRelLess(v.y,w.y))
+            ||  (isClose(v.x,w.x) && isClose(v.y,w.y) && isRelLess(v.z,w.z));
+}
+
 struct compareVector {
-    bool operator()(const vec3& v, const vec3& w) const {
+    bool operator()(const Vec3& v, const Vec3& w) const {
         return lessVector(v,w);
     }
 };
 
-void ReduceVertices(std::vector<vec3>& vertices_vector, std::vector<unsigned int>& indices_vector)
+void ReduceVertices(std::vector<Vec3>& vertices_vector, std::vector<unsigned int>& indices_vector)
 {
-    std::map<vec3, unsigned int, compareVector> vertices_map;
-    std::vector<vec3> new_vertices;
+    std::map<Vec3, unsigned int, compareVector> vertices_map;
+    std::vector<Vec3> new_vertices;
     for(unsigned int j = 0, l=0; j<indices_vector.size(); ++j){
         unsigned int i = indices_vector[j];
-        auto [it,success] = vertices_map.insert(std::pair<vec3, unsigned int>(
+        auto [it,success] = vertices_map.insert(std::pair<Vec3, unsigned int>(
                     vertices_vector[i], l));
         if(success) {
             new_vertices.push_back(vertices_vector[i]);
@@ -99,15 +125,21 @@ void ReduceVertices(std::vector<vec3>& vertices_vector, std::vector<unsigned int
     vertices_vector = new_vertices;
 }
 
-void CalculateNormals(std::shared_ptr<std::vector<vec3> > vertices_vector, std::shared_ptr<std::vector<unsigned int> > indices_vector,
-                      std::shared_ptr<std::vector<vec3> > normals_vector, std::shared_ptr<std::vector<unsigned int> > nindices_vector)
+void forgetSize( glm::vec3& v )
 {
-     LOG_ENGINE("Calculate Normals from Faces");
+    float l = glm::length(v);
+    float EPSILON_FLOAT = std::numeric_limits<float>::epsilon();
+    if(l>EPSILON_FLOAT) v /= l;
+}
 
+void CalculateNormals(std::shared_ptr<std::vector<Vec3> > vertices_vector, std::shared_ptr<std::vector<unsigned int> > indices_vector,
+                      std::shared_ptr<std::vector<Vec3> > normals_vector, std::shared_ptr<std::vector<unsigned int> > nindices_vector)
+{
+    LOG_ENGINE("Calculate Normals from Faces");
 
-    std::vector<vec3> face_normals;
-    for(unsigned int k=0; k<indices_vector->size(); k+=3)
-    face_normals.emplace_back(
+    std::vector<Vec3> face_normals;
+    for(unsigned int k=0; k < indices_vector->size(); k+=3)
+        face_normals.emplace_back(
                               normalize(
                               cross(
                                     (*vertices_vector)[(*indices_vector)[k+1]]-(*vertices_vector)[(*indices_vector)[k]],
@@ -115,12 +147,13 @@ void CalculateNormals(std::shared_ptr<std::vector<vec3> > vertices_vector, std::
                                   )
                              ));
 
-    //nindices_vector->resize(indices_vector->size());
+    nindices_vector->resize(indices_vector->size());
 
     for(unsigned int i = 0, max = vertices_vector->size(); i<max ; ++i){
-        std::vector<vec3> local_normals;
+        std::vector<Vec3> local_normals;
         std::vector<unsigned int>::iterator it = std::find(indices_vector->begin(), indices_vector->end(), i);
         int k;
+        
         while(it!=indices_vector->end()){
             k = std::distance(indices_vector->begin(), it);
             local_normals.push_back(face_normals[k/3]);
@@ -129,7 +162,7 @@ void CalculateNormals(std::shared_ptr<std::vector<vec3> > vertices_vector, std::
         }
         if(local_normals.empty()) continue;
         normals_vector->emplace_back(
-                    std::accumulate(local_normals.begin(), local_normals.end(), vec3(0.0f))
+                    std::accumulate(local_normals.begin(), local_normals.end(), Vec3(0.0f))
                     );
         forgetSize(normals_vector->back());
     }
